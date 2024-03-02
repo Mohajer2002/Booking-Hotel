f0010c2c69b828cd91ab5b90ec073ada6067b0f9
mohajer <senator.x.x85@gmail.com>
2024-03-02 13:25:18 +0330

    c3

--- src/hooks/useUrlLocation.js
+++
@@ -1,8 +0,0 @@
-import { useSearchParams } from "react-router-dom";
-
-export default function useUrlLocation() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-  return [lat, lng];
-}
